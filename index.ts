b314a970253f709d8090a0d76dd6f4902e83f784
theUniParse <theUniParse@gmail.com>
2023-09-20 02:48:43 +0100

    add --watch flag

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const { PORT } = process.env
+const server = Bun.serve({
+  port: +PORT! | 5000,
+  fetch(req, server) {
+    return new Response('hollo')
+  },
+})
+
+console.log(`listening on port ${server.port}`)
